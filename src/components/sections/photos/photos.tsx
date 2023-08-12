6c286023b1a9ee1ffcee30fb37c4afd4ee197bab
jasbel <asbel.elmec@gmail.com>
2023-08-12 17:09:42 -0400

    feat: photos

--- src/components/sections/photos/photos.tsx
+++ src/components/sections/photos/photos.tsx
@@ -3,38 +3,37 @@ import Section from "~/components/layout/section/section";
 import styles from "./photos.module.scss";
 import ItemPhoto from "./item-photo";
 
-const url = './images/photo2.png';
-const data = [{url},{url},{url},{url},{url},{url},];
+const url = "./images/photo2.png";
+const data = [{ url }, { url }, { url }, { url }, { url }, { url }];
 
 export default component$(() => {
   return (
-    <>
-      <figure>
-        <img src="./section/photos.png" />
-      </figure>
-      <Section id="photos" class={styles.section}>
-        <div class={styles.header}>
-          <div />
-          <h2 class={styles.title}>Fotos</h2>
-          <div class={styles["button-container"]}>
-            <button>{"<"}</button>
-            <button>{">"}</button>
-          </div>
+    <Section id="photos" class={styles.section}>
+      <div class={styles.header}>
+        <div />
+        <h2 class={styles.title}>Fotos</h2>
+        <div class={styles["button-container"]}>
+          <button>
+            <i class="fa-solid fa-chevron-left"></i>
+          </button>
+          <button>
+            <i class="fa-solid fa-chevron-right"></i>
+          </button>
         </div>
+      </div>
 
-        <div class={styles["item-container"]}>
-          {data.map((d) => (
-            <>
-              <ItemPhoto url={d.url} />
-            </>
-          ))}
+      <div class={styles["item-container"]}>
+        {data.map((d) => (
+          <>
+            <ItemPhoto url={d.url} />
+          </>
+        ))}
+      </div>
+      <div class={styles.progress__wrap}>
+        <div class={styles.progress}>
+          <div class={styles.progress__item}></div>
         </div>
-        <div class={styles.progress__wrap}>
-          <div class={styles.progress}>
-            <div class={styles.progress__item}></div>
-          </div>
-        </div>
-      </Section>
-    </>
+      </div>
+    </Section>
   );
 });
